7b0201c3c1bf3c2b538f3b7d845d1d27ede54c23
chan9yu <dev.cgyeo@gmail.com>
2025-05-02 17:17:26 +0900

    chore: Tailwind CSS 초기 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [],
+	plugins: [tailwindcss()],
 	server: {
 		port: 3003
 	}
